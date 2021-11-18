0ee34e3109525f643d666be19fe38856cee302d7
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2021-11-17 21:40:17 -0600

    Minor changes

--- py/python.py
+++ py/python.py
@@ -3,3 +3,9 @@ def python(args:list = []):
 	# AYO, WHY AIN'T THIS IMPLEMENTED YET!
 	# Reminder: Use PyTAS's implementation of this feature
 	return
+
+# Use exec() and install user-input to root. Yes, I'm trusting users with root access.
+# Should I? Yeah I guess.
+# Remember to add a check for administrator flags. Maybe implement sudo command
+# as well.
+# MCMi460 out!
