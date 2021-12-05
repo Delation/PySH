81aa54c4f7b54217415125180ac053e561b96654
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2021-12-05 13:34:40 -0600

    Add pynput source for key inputs

--- cli.py
+++ cli.py
@@ -11,7 +11,8 @@ class User:
 	flags: list
 	password: str = None
 
-import os, sys
+import os, sys, time
+from py.utility import Utility
 
 shell = 'PySH'
 bin = './py/'
@@ -22,6 +23,16 @@ hashseed = os.getenv('PYTHONHASHSEED')
 if not hashseed:
 	os.environ['PYTHONHASHSEED'] = '0'
 	os.execv(sys.executable, [sys.executable] + sys.argv)
+
+from pynput.keyboard import Key, Controller, Listener
+
+keyboard = Controller()
+location = ''
+buffer = None
+history = []
+cursor = 0
+get_input = True
+listener = None
 
 class Login():
 	def create_log_file(self,path:str = bin+'users'):
@@ -43,9 +54,7 @@ class Login():
 			i.write(f'{account.username}|{",".join(account.flags)}|{account.password}\n')
 
 def main():
-	global account
-	global accounts
-	global flags
+	global account, accounts, flags, location, buffer, history, get_input, listener
 	if Login().create_log_file():
 		Login().get_login()
 	else:
@@ -71,6 +80,12 @@ def main():
 		main()
 		return
 
+	listener = Listener(
+		suppress=True,
+		on_press=on_press,
+		on_release=on_release)
+	listener.start()
+
 	commands = {}
 
 	i = []
@@ -87,9 +102,33 @@ def main():
 	commands['accounts'] = accounts
 	commands['account'] = account
 	while log:
-		location = commands['os'].getcwd()
+		get_input = True
+		if buffer:history.append(buffer.replace(location,''))
+		location = f'{os.path.abspath(commands["os"].getcwd())}:~ {account.username}$ '
 		work = False
-		cmd = input(f'{os.path.abspath(location)}:~ {account.username}$ ').lstrip().rstrip().split(' ')
+		buffer = f'{location}'
+		print(buffer,end='',flush=True)
+		while get_input:
+			if not commands['account']:
+				log = False
+				listener.stop()
+				listener = None
+				commands['clear']()
+				main()
+				return
+			if location not in buffer:
+				buffer = f'{location}'
+				refreshBuffer()
+			if not listener:
+				input('\nPlease press enter when you wish to enter the console.')
+				listener = Listener(
+					suppress=True,
+					on_press=on_press,
+					on_release=on_release)
+				listener.start()
+				refreshBuffer()
+		print('')
+		cmd = buffer.replace(location,'').lstrip().rstrip().split(' ')
 		for i in commands:
 			if i in ('__builtins__','system','os','inspect','platform') or not callable(commands[i]):
 				continue
@@ -105,8 +144,57 @@ def main():
 				break
 		if not work:
 			print(f'{shell}: command not found: {cmd[0]}')
+	listener.stop()
+	listener = None
 	main()
 	return
+
+def on_press(key):
+	global buffer, get_input, cursor, listener
+	if not get_input:
+		return
+	if key == Key.backspace:
+		buffer = buffer[:-1]
+		refreshBuffer()
+	elif key == Key.space:
+		print(' ',end='',flush=True)
+		buffer = buffer + ' '
+	elif key == Key.enter:
+		get_input = False
+	elif key == Key.up:
+		if not len(history) < abs(cursor - 1):
+			cursor -= 1
+			buffer = f'{location}{history[cursor]}'
+			refreshBuffer()
+	elif key == Key.down:
+		if cursor + 1 < 0:
+			cursor += 1
+			buffer = f'{location}{history[cursor]}'
+			refreshBuffer()
+		elif cursor + 1 == 0:
+			cursor += 1
+			buffer = f'{location}'
+			refreshBuffer()
+	elif key == Key.esc:
+		listener.stop()
+		listener = None
+		refreshBuffer()
+	else:
+		try:
+			print(key.char,end='',flush=True)
+			buffer = buffer + key.char
+		except Exception:
+			pass
+
+def on_release(key):
+	global buffer, get_input
+	if not get_input:
+		return
+
+def refreshBuffer():
+	print('',end='\r',flush=True)
+	print(' '*Utility().get_size()[1],end='\r',flush=True)
+	print(buffer,end='',flush=True)
 
 if __name__ == "__main__":
 	main()
--- py/logout.py
+++ py/logout.py
@@ -1,4 +1,7 @@
 #!/bin/pysh
 def logout(args:list = []):
 	Utility().check_args(args)
-	return 'Will be implemented later - need to sleep'
+	global account
+	username = account.username
+	account = None
+	return f'User {username} has been logged out.'
