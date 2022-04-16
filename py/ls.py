54e94c5f3ed235df3e4306d91677cf30e0e21989
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-04-15 20:36:03 -0500

    Update ls.py

--- py/ls.py
+++ py/ls.py
@@ -1,10 +1,9 @@
 def ls(*args):
-	print('# Notice of usage - unfinised command')
-	# Currently, since I'm a couch potato, this command simply just redirects into
-	# the system's implementation of it
-	# No regard for whether or not the command actually exists, might I add.
-	return os.system(f'ls {" ".join(args)}')
-	# In-work implementation
+	if len(args) > 0:
+		print('# Notice of usage - unfinised command')
 	lines, columns = Utility().get_size()
-	o = ' '.join(os.listdir(os.getcwd()))
-	return '\n'.join(o[i:i+columns] for i in range(0, len(o), columns))
+	o = os.listdir(os.getcwd())
+	distance = ' '*int(columns/10)
+	length = int(columns/len(max(o, key=len)))
+	o = distance.join([ o[i] + (' '*(length - len(o[i]))) for i in range(len(o)) ])
+	return '\n'.join([ o[i:i+columns].strip() for i in range(0, len(o), columns) ])
